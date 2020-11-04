96850257597db94bfb51ab925c63050a3f24c67f
emilio richter <emiliorg@me.com>
2020-11-04 16:02:21 -0700

    <commit message>

---
+++ Copia de main.js
@@ -0,0 +1,38 @@
+// Image switcher code
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'Unknown.png') {
+    myImage.setAttribute ('src','original.jpeg');
+  } else {
+    myImage.setAttribute ('src','Unknown.png');
+  }
+}
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'The odin project, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'The odin project, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
